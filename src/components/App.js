b972d824ff9ed52761089914e11810ae814360e5
Michael Cook <mcook0775@gmail.com>
2020-06-08 22:17:43 -0400

    Second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
 /*
 Top level application component
+Flickr API docs <https://www.flickr.com/services/api/>
+<https://www.flickr.com/services/api/flickr.photos.search.html>
+tut: <https://www.w3resource.com/API/flickr/tutorial.php>
 */
 
-import React, { Component } from "react"
-import Search from "./Search"
-import SearchNav from "./SearchNav"
-import NotFound from "./NotFound"
+import React, { Component } from 'react'
+import Search from './Search'
+import SearchNav from './SearchNav'
+import NotFound from './NotFound'
+import apiKey from '../config.js'
+import axios from 'axios'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
+    this.photos = null;
+  }
+
+  /**
+   * Seach for photos matching a search term or set of search terms
+   */
+  search = (query = "animals") => {
+    const baseURL = 'https://www.flickr.com/services/rest/';
+    const method = 'flickr.photos.search'
+    axios.get(`${baseURL}?method=${method}&text=${query}&api_key=${apiKey}&format=json`)
+      .then(res => console.log(res))
+      .catch(err => console.log("Something went wrong."));
+  }
+
+  componentDidMount() {
+    this.search();
   }
 
   render() {
