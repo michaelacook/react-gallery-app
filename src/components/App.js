5af04c579e8d9e458c0d1a7f7e1244f798a685af
Michael Cook <mcook0775@gmail.com>
2020-06-08 22:24:37 -0400

    Update App

--- src/components/App.js
+++ src/components/App.js
@@ -5,32 +5,39 @@ Flickr API docs <https://www.flickr.com/services/api/>
 tut: <https://www.w3resource.com/API/flickr/tutorial.php>
 */
 
-import React, { Component } from 'react'
-import Search from './Search'
-import SearchNav from './SearchNav'
-import NotFound from './NotFound'
-import apiKey from '../config.js'
-import axios from 'axios'
+import React, { Component } from "react"
+import Search from "./Search"
+import SearchNav from "./SearchNav"
+import NotFound from "./NotFound"
+import apiKey from "../config.js"
+import axios from "axios"
 
 export default class App extends Component {
   constructor(props) {
     super(props)
-    this.photos = null;
+    this.photos = null
+    this.loading = true
   }
 
   /**
    * Seach for photos matching a search term or set of search terms
    */
   search = (query = "animals") => {
-    const baseURL = 'https://www.flickr.com/services/rest/';
-    const method = 'flickr.photos.search'
-    axios.get(`${baseURL}?method=${method}&text=${query}&api_key=${apiKey}&format=json`)
-      .then(res => console.log(res))
-      .catch(err => console.log("Something went wrong."));
+    const baseURL = "https://www.flickr.com/services/rest/"
+    const method = "flickr.photos.search"
+    axios
+      .get(
+        `${baseURL}?method=${method}&text=${query}&api_key=${apiKey}&format=json`
+      )
+      .then((res) => {
+        console.log(res)
+        this.setState({ loading: false })
+      })
+      .catch((err) => console.log("Something went wrong."))
   }
 
   componentDidMount() {
-    this.search();
+    this.search()
   }
 
   render() {
